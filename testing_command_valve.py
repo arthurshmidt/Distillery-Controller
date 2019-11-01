0dc8bf7c913ca5a3ffd1ad2ad9626c6dc0f946b2
arthurshmidt <47873761+arthurshmidt@users.noreply.github.com>
2019-11-01 17:20:33 -0400

    Corrected errors.

--- testing_command_valve.py
+++ testing_command_valve.py
@@ -1,11 +1,11 @@
 # Testing file fof the PI-SPI-DIN-AO Analog Output Module that communicates to
 # Raspberry Pi via the SPI bus.
 from time import sleep
-from widgetlords.pi_spi_din import *
+from widgetlords.pi_spi import *
 # from widgetlords import *
 
 init()
-outputs = Mod4AO()
+outputs = Mod2AO()
 
 def percent_to_da(valve_percent):
     da_signal = ((4000-800)/(100-0)) * valve_percent + 800
@@ -14,7 +14,7 @@ def percent_to_da(valve_percent):
 # Main program loop
 while True:
     valve_percent = int(input("Enter valve position in %: "))
-    da_signal = percent_to_da(valve_percent)
+    da_signal = int(percent_to_da(valve_percent))
 
     print("You entered {}% = {}da".format(valve_percent,da_signal))
     outputs.write_single(0,da_signal)     # 800 = 4ma ; 4000 = 20ma
