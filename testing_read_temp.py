34f9d5aceaab2b9b1c08c5d165faed2c421aedd1
arthurshmidt <47873761+arthurshmidt@users.noreply.github.com>
2019-11-05 16:55:38 -0500

    Added Global var for Temperature Calibration

--- testing_read_temp.py
+++ testing_read_temp.py
@@ -5,6 +5,8 @@ from widgetlords.pi_spi_din import *
 from widgetlords import *
 from os import system
 
+temp_calibration_factor = 3.0
+
 init()
 inputs = Mod8AI(ChipEnable.CE0)
 
@@ -21,21 +23,24 @@ def read_basic():
         print(inputs.read_single(0))
         sleep(0.5)
 
+# ************************************************************************* #
+#                           Main Section                                    #
+# ************************************************************************* #
+
 while True:
     clear_screen()
     d_return = inputs.read_single(0)         # Dephleg Return
     c_return = inputs.read_single(1)         # Condensor Return
     d_supply = inputs.read_single(2)         # Dephleg Supply
-    c_supply = inputs.read_single(3)         # Condensor Supply 
-        
-    d_return_c = steinhart_hart(10000, 3380, 4095, d_return) - 3.0
-    c_return_c = steinhart_hart(10000, 3380, 4095, c_return) - 3.0
-    d_supply_c = steinhart_hart(10000, 3380, 4095, d_supply) - 3.0
-    c_supply_c = steinhart_hart(10000, 3380, 4095, c_supply) - 3.0
+    c_supply = inputs.read_single(3)         # Condensor Supply
+
+    d_return_c = steinhart_hart(10000, 3380, 4095, d_return) - temp_calibration_factor
+    c_return_c = steinhart_hart(10000, 3380, 4095, c_return) - temp_calibration_factor
+    d_supply_c = steinhart_hart(10000, 3380, 4095, d_supply) - temp_calibration_factor
+    c_supply_c = steinhart_hart(10000, 3380, 4095, c_supply) - temp_calibration_factor
 
     print("Deph Supply: {0:.2f}F".format(celcius_to_fahrnheit(d_supply_c)))
     print("Deph Return: {0:.2f}F".format(celcius_to_fahrnheit(d_return_c)))
     print("Cond Supply: {0:.2f}F".format(celcius_to_fahrnheit(c_supply_c)))
     print("Cond Return: {0:.2f}F".format(celcius_to_fahrnheit(c_return_c)))
     sleep(2)
-
